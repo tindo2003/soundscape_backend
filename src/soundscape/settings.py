c81d80a401b183c7b5477b25bc52650c4088ef99
Tin Do <dothanhtin003@gmail.com>
2025-05-12 21:24:48 -0400

    changed settings

--- src/soundscape/settings.py
+++ src/soundscape/settings.py
@@ -31,7 +31,16 @@ SECRET_KEY = config("DJANGO_SECRET_KEY", cast=str)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DJANGO_DEBUG", cast=bool, default=True)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    ".railway.app"
+]
+if DEBUG:
+    ALLOWED_HOSTS = ["*"]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://*.railway.app",
+    "https://*.railway.app",
+]
 
 
 # Application definition
